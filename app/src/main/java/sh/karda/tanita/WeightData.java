da279aa76c1c003efebd9cddd749f232737a8bae
Anders Kardash <anders.kardash@passport.com>
2018-06-01 22:07:36 +0200

    Writing to file

---
+++ app/src/main/java/sh/karda/tanita/WeightData.java
@@ -0,0 +1,14 @@
+package sh.karda.tanita;
+
+public class WeightData {
+    public double Weight;
+    public double Bmi;
+    public double Water;
+    public double Muscle;
+    public int rating;
+    public int rest;
+    public int age;
+    public double bone;
+    public int viscelar;
+
+}
